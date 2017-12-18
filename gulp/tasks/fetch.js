cdf7b62ce62bd392cf5201ad7d9a4c848ba39e30
Moritz Rebbert <moritz.rebbert@udo.edu>
2017-12-18 23:55:03 +0100

    fetched version 4.5.0

--- gulp/tasks/fetch.js
+++ gulp/tasks/fetch.js
@@ -1,5 +1,5 @@
 //fetches newest version from http://ledoux.io/bitsy/editor.html hompage
-var gulp     = require('gulp');
+var gulp = require('gulp');
 var wget = require('wgetpage');
 var download = require('gulp-download-stream');
 
@@ -8,19 +8,23 @@ var URL = 'http://ledoux.io/bitsy/';
 var INDEX = 'editor.html';
 var DEST = 'app/';
 var MISSING = [
-    'exportStyleFixed.css',
-    'exportStyleFull.css',
-    'exportTemplate.html'
+    'style/exportStyleFixed.css',
+    'style/exportStyleFull.css',
+    'other/exportTemplate.html',
+    'font/google-nunito-latin.woff2',
+    'font/google-material-icons.woff2'
 ];
 
-gulp.task('fetch-editor', [], function(cb){
-    wget(URL+INDEX, DEST,cb);
+gulp.task('fetch-editor', [], function (cb) {
+    wget(URL + INDEX, DEST, cb);
     //gulp.src(['app/**']).pipe(function(a){console.log(a)})
 });
 
-gulp.task('fetch-missing', [], function(){
-   var missingUrls =  MISSING.map(function(name){return URL + name;});
-   return download(missingUrls).pipe(gulp.dest("app/"));
+gulp.task('fetch-missing', [], function () {
+    var missingUrls = MISSING.map(function (name) {
+        return {url: URL + name, file: name};
+    });
+    return download(missingUrls).pipe(gulp.dest("app/"));
 });
 
 gulp.task('fetch', ['fetch-missing', 'fetch-editor']);
